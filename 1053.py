8409495cf0204ba693c8dd2eb14afd2ee3bf3fc1
woo-ara <woo.ara00@gmail.com>
2022-11-06 16:29:10 +0900

    22.11.03 백준 문제풀이-정답

--- 1053.py
+++ 1053.py
@@ -6,8 +6,56 @@
 # 2) 다른 문자로 교환
 # 3) 서로 교환 (한번만) - 이거먼저
 
+
 from itertools import combinations
 
+
+def swap_char(a, b, st):
+    c = st[b]
+    st = st[:b] + st[a] + st[b+1:]
+    st = st[:a] + c + st[a+1:]
+    return st
+
+
+def factory(start_idx, end_idx, string):
+    if cache[start_idx][end_idx] != -1:
+        return cache[start_idx][end_idx]
+
+    while start_idx < end_idx:
+        if string[start_idx] == string[end_idx]:
+            start_idx += 1
+            end_idx -= 1
+        else:
+            break
+
+    if start_idx >= end_idx:
+        return 0
+
+    ret = 1000000000
+    ret = min(
+        ret,
+        factory(start_idx + 1, end_idx, string) + 1,
+        factory(start_idx, end_idx - 1, string) + 1,
+        factory(start_idx + 1, end_idx - 1, string) + 1
+    )
+    cache[start_idx][end_idx] = ret
+    return ret
+
+
+S = input()
+s_len = len(S)
+change_two = list(combinations([i for i in range(s_len)], 2))
+answer = 1000000000
+cache = [[-1]*51 for _ in range(51)]
+answer = min(answer, factory(0, s_len-1, S))
+for com in change_two:
+    cache = [[-1] * 51 for _ in range(51)]
+    s = swap_char(com[0], com[1], S)
+    answer = min(answer, factory(0, s_len-1, s) + 1)
+print(answer)
+
+'''
+# 틀린 답안
 def rec(l, r):
     if (l >= r):
         return 0
@@ -39,15 +87,7 @@ for i in range(letter_len):
         letter[j], letter[i] = letter[i], letter[j] #swap
 
 print(answer)
-
-
-
-
-
-
-
-
-
+'''
 
 '''
 from itertools import combinations
