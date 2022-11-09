e811f4cf703040239167e044910e8c798284d71d
woo-ara <woo.ara00@gmail.com>
2022-11-10 04:22:19 +0900

    22.11.10 백준 문제풀이

--- 1182.py
+++ 1182.py
@@ -5,13 +5,13 @@
 
 from itertools import chain, combinations
 
-n, s = map(int, input().split())    #정수개수, 합
+n, s = map(int, input().split())
 num_list = list(map(int, input().split(maxsplit=n)))
-com_list = list(chain.from_iterable(combinations(num_list, r) for r in range(1, len(num_list) + 1)))
+com_list = list(chain.from_iterable(combinations(num_list, r) for r in range(1, len(num_list) + 1)))    #모든 조합 구하기
 
 sum_list = []
 cnt = 0
-for i in range(len(com_list)):
+for i in range(len(com_list)):  #조합의 합이 s 이면 cnt++
     if sum(com_list[i]) == s:
         cnt += 1
 print(cnt)
---
+++ 1759.py
@@ -0,0 +1,132 @@
+'''
+백준 1759 암호 만들기
+22.11.10 우아라
+'''
+
+from itertools import *
+
+l, c = map(int, input().split())    #알파벳개수, 문자종류
+c_letter = list(input().split(maxsplit=c))
+
+vow = ['a', 'e', 'i', 'o', 'u'] #vowel
+vow_len = 0
+vow_list = []
+for i in vow:
+    if i in c_letter:
+        vow_len += 1
+        vow_list.append(i)
+
+con_len = l - vow_len #consonant
+con_list = list(set(c_letter) - set(vow_list))
+
+vow_ans = list(combinations(vow_list, 1))
+con_ans = list(combinations(con_list, 2))
+
+ans_list = []
+for i in vow_ans:
+    ans = []
+    for j in con_ans:
+        k_list = list(set(c_letter) - set(i) - set(j))
+        k_ans = list(combinations(k_list, (l - 3)))
+        for k in k_ans:
+            ans = list(i + j + k)
+            ans.sort()
+            ans_list.append(''.join(ans))
+
+ans_list = list(set(ans_list))
+ans_list.sort()
+
+for i in ans_list:
+    print(i)
+
+
+
+
+
+
+
+'''
+vow = ['a', 'e', 'i', 'o', 'u'] #vowel
+alphabet = list(ascii_lowercase)
+vow_len = 0
+vow_list = []
+for i in vow:
+    if i in c_letter:
+        vow_len += 1
+        vow_list.append(i)
+        
+con_len = l - vow_len #consonant
+con_list = list(set(c_letter) - set(vow_list))
+
+vow_ans = []
+con_ans = []
+# for i in range(vow_len):
+vow_ans.append(list(combinations(vow_list, 1)))
+# for i in range(con_len):
+con_ans.append(list(combinations(con_list, 2)))
+'''
+
+
+
+
+
+
+
+
+
+
+
+'''
+ans = []
+if vow_len == 1:
+    a = Counter(c_letter)
+    b = a.most_common(l - 1)
+    for i in range(len(b)):
+        ans = vow_list[0] + b[i]
+        ans.sort()
+        print(ans)
+else:
+    while con_len < 2:
+        for i in range(l):
+
+
+
+
+
+v = ['a', 'e', 'i', 'o', 'u']
+alphabet = list(ascii_lowercase)
+
+v_len = 0
+v_list = []
+for i in v:
+    if i in c_letter:
+        v_len += 1
+        v_list.append(i)
+        
+ans = []
+def answer(idx):
+    for i in range(c):
+        if c_letter[i] == 'a' | 'e' | 'i' | 'o' | 'u':
+            ans.append(v_list[i])
+        else:
+            ans.append(v_list[i])
+        if len(ans) == l:
+            print(ans)
+            del ans[idx:]
+
+for j in range(len())
+
+#v_len > 1 이면 하나만 출력하거나 여러개 가능
+
+#v_len == 1이면 무조건 출력, 앞, 뒤에오는 것 중 l-1개 출력 가능
+if v_len == 1:
+    ans.append(v_list[0])
+    idx = alphabet.index(v_list[0])
+    left_list = []
+    right_list = []
+    for i in range(idx):
+        left_list.append(list(combinations(alphabet[:idx], i)))
+        right_list.append(list(combinations(alphabet[idx:], i)))
+'''
+
+
