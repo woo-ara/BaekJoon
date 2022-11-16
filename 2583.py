5fa45d50ea66d7fd94a427d126315f267105efbb
woo-ara <woo.ara00@gmail.com>
2022-11-16 23:35:59 +0900

    22.11.16 백준 문제풀이

---
+++ 2583.py
@@ -0,0 +1,18 @@
+'''
+백준 2583 영역 구하기
+22.11.16 우아라
+'''
+
+m, n, k = map(int, input().split()) #모눈종이 오른쪽 위 꼭짓점, 입력 k개 직사각형
+
+k_list = []
+for i in range(k):
+    k_list.append(map(int, input().split()))
+
+
+
+
+
+
+
+
