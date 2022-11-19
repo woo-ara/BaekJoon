d999d7267b72d3add622a7701703316df82da860
woo-ara <woo.ara00@gmail.com>
2022-11-19 16:58:02 +0900

    22.11.19 백준 문제풀이

--- 1926.py
+++ 1926.py
@@ -3,6 +3,46 @@
 22.11.18 우아라
 '''
 
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000000)
+
+n,m = map(int,input().split())
+graph = []
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+def dfs(x,y):
+    global cnt
+    cnt += 1
+    graph[x][y] = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if -1<nx<n and -1<ny<m and graph[nx][ny]==1:
+            dfs(nx,ny)
+
+num_list = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            cnt=0
+            dfs(i,j)
+            num_list.append(cnt)
+
+if len(num_list)==0:
+    print(len(num_list))
+    print(0)
+else:
+    print(len(num_list))
+    print(max(num_list))
+
+
+
+
+'''
 from collections import deque
 
 n, m = map(int, input().split())
@@ -47,3 +87,5 @@ if len(paint) == 0:
 else:
     print(len(paint))
     print(max(paint))
+'''
+
